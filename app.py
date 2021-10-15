54c27e20ebdde2e92e12100fd456c1a10560ba87
aleksandr.gogolev <aleksandr.gogolev@simbirsoft.com>
2021-10-15 21:15:41 +0300

    init commit

---
+++ app.py
@@ -0,0 +1,102 @@
+#!flask/bin/python
+from flask import Flask
+from flask import jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import url_for
+
+app = Flask(__name__)
+
+tasks = [
+        {
+            'id': 1,
+            'title': u'learn postgres',
+            'description': u'start learning postgres',
+            'done': False
+        },
+        {
+            'id': 2,
+            'title': u'learn Python',
+            'description': u'start learning Python',
+            'done': False
+        },
+        {
+            'id': 3,
+            'title': u'learn Postman',
+            'description': u'start learning Postman api',
+            'done': False
+        }
+    ]
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+@app.route('/')
+def index():
+    return "привет Михаил пройдись по командам из файла README"
+
+@app.route('/rae/api/tasks', methods=['GET'])
+def get_tasks():
+    # return jsonify({'tasks': tasks})
+    return jsonify({'tasks': list(map(make_public_task, tasks))})
+
+@app.route('/rae/api//tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/rae/api/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/rae/api/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/rae/api/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    print ("Raevskiy API by Gogolev")
